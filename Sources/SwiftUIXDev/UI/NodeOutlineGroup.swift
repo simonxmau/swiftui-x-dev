51a1d57fe7e6ed357db82e0a289581de83c995d1
Simon Mau <git@simonmau.me>
2025-12-10 03:50:14 +0800

    feat: add NodeOutlineGroup view for hierarchical data representation
    - Introduced NodeOutlineGroup struct to create a collapsible outline view for displaying hierarchical data structures in SwiftUI.
    - Supports expandable/collapsible functionality for nodes with child elements, enhancing UI interactivity.

---
+++ Sources/SwiftUIXDev/UI/NodeOutlineGroup.swift
@@ -0,0 +1,35 @@
+//
+//  NodeOutlineGroup.swift
+//  swiftui-x-dev
+//
+//  Created by Simon Mau on 2025/12/10.
+//  From: https://stackoverflow.com/questions/62832809/list-or-outlinegroup-expanded-by-default-in-swiftui
+//
+import SwiftUI
+
+public struct NodeOutlineGroup<Node, Content>: View where Node: Hashable, Node: Identifiable, Node: CustomStringConvertible, Content: View {
+    let node: Node
+    let childKeyPath: KeyPath<Node, [Node]?>
+    @State var isExpanded: Bool = true
+    let content: (Node) -> Content
+    
+    public var body: some View {
+        if let children = node[keyPath: childKeyPath], !children.isEmpty {
+            DisclosureGroup(
+                isExpanded: $isExpanded,
+                content: {
+                    if isExpanded {
+                        ForEach(children, id: \.self) { childNode in
+                            NodeOutlineGroup(node: childNode, childKeyPath: childKeyPath, isExpanded: isExpanded, content: content)
+                        }
+                    }
+                },
+                label: {
+                    content(node)
+                }
+            )
+        } else {
+            content(node)
+        }
+    }
+}
